12324004ae6334a7bec06baf32106187d07e25e2
Oon Chu Lip <oonchulip97@outlook.com>
2019-09-16 18:03:57 -0700

    [major] Add setup files

---
+++ vim_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install vim and vim-plug
+apt install vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install .vimrc file
+$CURR_DIR/vim_dotfiles > $HOME/.vimrc
+
+# Run vim-plug to install all necessary plugins
+vim +'PlugInstall --sync' +qa
+
