c5c8f73a467695ab6ef69a5821e19d6fe8324552
Oon Chu Lip <oonchulip97@outlook.com>
2026-03-26 07:48:53 -0700

    [major] Upgrade goto function, autocompletion, syntax checking

--- vim_setup.sh
+++ vim_setup.sh
@@ -6,9 +6,12 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo apt install vim curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install dependencies
+sudo apt install exuberant-ctags  # switch to universal-ctags for ubuntu 20.0 or above
+sudo apt install cppcheck flake8 shellcheck  # linter
+
 # Install .vimrc file
 cp $CURR_DIR/vim_dotfiles $HOME/.vimrc
 
 # Run vim-plug to install all necessary plugins
 vim +'PlugInstall --sync' +qa
-
