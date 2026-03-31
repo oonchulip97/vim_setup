661307e596cdf5650f939ba8a4792c8540414726
Oon Chu Lip <chulip.oon@aandkrobotics.com>
2026-03-30 17:00:26 -0700

    [minor] continue tuning

--- vim_setup.sh
+++ vim_setup.sh
@@ -6,6 +6,9 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo apt install vim curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install dependencies
+sudo apt install exuberant-ctags  # switch to universal-ctags for ubuntu 20.0 or above
+
 # Install .vimrc file
 cp $CURR_DIR/vim_dotfiles $HOME/.vimrc
 
