724a669d5480b80ddfe50ca4912033fcac1012ae
Oon Chu Lip <chulip.oon@aandkrobotics.com>
2026-04-08 08:50:25 -0700

    [bugfix] Download py2-compatible cpplint

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,7 +4,6 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install dependencies
 sudo apt install python-pip  # switch to pip3 for ubuntu 20.0 or above
-python -m pip install --user --upgrade pip setuptools wheel
 
 # Install vim and vim-plug
 sudo apt install vim curl
@@ -12,8 +11,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 
 # Install dependencies
 sudo apt install exuberant-ctags  # switch to universal-ctags for ubuntu 20.0 or above
-sudo apt install cppcheck flake8 shellcheck  # linter
-pip install --user cpplint  # linter
+pip install --user "cpplint<1.3.0"  # linter switch to python3 compatible version for ubuntu 20.0 or above
 
 # Install .vimrc file
 cp $CURR_DIR/vim_dotfiles $HOME/.vimrc
