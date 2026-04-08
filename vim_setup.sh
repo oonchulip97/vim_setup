daa0cd5243d317c67f73e864f901601f102f990f
Oon Chu Lip <oonchulip97@outlook.com>
2026-04-07 20:16:43 -0700

    [minor] Add cpplint

--- vim_setup.sh
+++ vim_setup.sh
@@ -2,6 +2,10 @@
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Install dependencies
+sudo apt install python-pip  # switch to pip3 for ubuntu 20.0 or above
+python -m pip install --user --upgrade pip setuptools wheel
+
 # Install vim and vim-plug
 sudo apt install vim curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -9,6 +13,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # Install dependencies
 sudo apt install exuberant-ctags  # switch to universal-ctags for ubuntu 20.0 or above
 sudo apt install cppcheck flake8 shellcheck  # linter
+pip install --user cpplint  # linter
 
 # Install .vimrc file
 cp $CURR_DIR/vim_dotfiles $HOME/.vimrc
